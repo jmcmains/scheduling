1b28227eaaac9ceb19be7c9112519416c3f7893e
jmcmains <jmcmains@gmail.com>
2013-01-06 19:07:07 -0500

    fix gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduling::Application.config.secret_token = 'f8c11bd2f2171940fee91d589df434228b0a9e0e5e19b84b7b55942f07e8c4704f6c0d68c6cdda6227819345080273c8f39ac1d15b22cd3c730062baaa9a737b'
