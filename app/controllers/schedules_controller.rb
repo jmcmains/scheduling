f37cef87c65ac01c78169fa1a169d7c065990081
jmcmains <jmcmains@gmail.com>
2013-01-07 21:16:30 -0500

    fixed project creation

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,16 +4,16 @@ class SchedulesController < ApplicationController
 		@project.save
     if params[:go]
 		  current_project=Schedule.find_by_end_at(nil)
-		  @project.featured=true
+		  @project.update_attributes(featured: true)
 		  if current_project
 		  	current_project.update_attributes(end_at:5.hours.ago)
 		  end
 		  Schedule.create!(project_id:@project.id,start_at:5.hours.ago)
 		elsif params[:del]
-			@project.featured=false
+			@project.update_attributes(featured: false)
 		else
 			current_project=Schedule.find_by_end_at(nil)
-			@project.featured=true
+			@project.update_attributes(featured: true)
 			if current_project
 				current_project.update_attributes(end_at:5.hours.ago)
 			end
