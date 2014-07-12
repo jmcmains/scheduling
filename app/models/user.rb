ab95d3333093cb187b875741863f7defc2064dba
Jason McMains <jmcmains@gmail.com>
2014-07-12 15:28:30 -0400

    still fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
-
 
   has_many :features, dependent: :destroy
   accepts_nested_attributes_for :features
