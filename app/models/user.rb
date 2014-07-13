71c30b00bcf6532535bb48fced5e7f91f38026b0
Jason McMains <jmcmains@gmail.com>
2014-07-13 18:59:03 -0400

    fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,54 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
 
 
-  has_secure_password
-  
-  before_save { |user| user.email = email.downcase }
-  before_save :create_remember_token
-  
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
-  validates :time_zone, 
-    inclusion: { 
-      in: ActiveSupport::TimeZone.zones_map(&:name).keys 
-    } 
   has_many :features, dependent: :destroy
   accepts_nested_attributes_for :features
   
   def working?
-  	!Schedule.find_by_end_at_and_user_id(nil,id).blank?
+   !Schedule.find_by_end_at_and_user_id(nil,id).blank?
   end
 
+  def self.from_omniauth(auth)
+   where(auth.slice(:provider, :uid)).first_or_create do |user|
+   user.provider = auth.provider
+   user.uid = auth.uid
+   user.name = auth.info.nickname
+   end
+  end
   
+  def self.new_with_session(params, session)
+   if session["devise.user_attributes"]
+new session["devise.user_attributes"] do |user|
+user.attributes = params
+user.valid?
+   end
+   else
+   super
+   end
+  end
+  
+  def password_required?
+   super && provider.blank?
+  end
+  
+  def update_with_password(params, *options)
+   if encrypted_password.blank?
+   update_attributes(params,*options)
+   else
+   super
+   end
+  end
+  
+  def email_required?
+   super && provider.blank?
+  end
   private
   
-  	def create_remember_token
-  		self.remember_token = SecureRandom.urlsafe_base64
-  	end
+   def create_remember_token
+   self.remember_token = SecureRandom.urlsafe_base64
+   end
 end
